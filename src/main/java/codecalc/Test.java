bd92dbe1edbbda9c960c2ae6232c4de317cf4d9e
kmnemon <kmnemon@163.com>
2021-07-02 14:12:14 +0800

    init gradle

---
+++ src/main/java/codecalc/Test.java
@@ -0,0 +1,11 @@
+package codecalc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+    private static final Logger LOG = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+        LOG.info("what");
+    }
+}
