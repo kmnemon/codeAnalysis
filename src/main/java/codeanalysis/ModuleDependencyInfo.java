11c5cd720b8811e9e4cc237ae3b0277633ade8a7
kmnemon <kmnemon@163.com>
2021-07-05 19:11:23 +0800

    add module and project dependencyInfo.class

--- src/main/java/codeanalysis/ModuleDependencyInfo.java
+++ src/main/java/codeanalysis/ModuleDependencyInfo.java
@@ -4,18 +4,41 @@ import java.util.List;
 import java.util.Objects;
 
 public class ModuleDependencyInfo {
-    private List<String> fanIn;
-    private List<String> fanOut;
-    private Integer instability;
     private String moduleName;
+    private List<ClassDependencyInfo> moduleClasses;
 
+    private List<ClassDependencyInfo> fanInClasses;
+    private List<ClassDependencyInfo> fanOutClasses;
+    private Integer instability;
+
+    private Integer numOfClasses;
+    private Integer numOfAbsAndInterfaceClasses;
+    private Integer abstractness;
+
+    private ModuleDependencyInfo(String moduleName, List<ClassDependencyInfo> moduleClasses){
+        this.moduleName = moduleName;
+        this.moduleClasses = List.copyOf(moduleClasses);
+    }
+
+    public static ModuleDependencyInfo create(String moduleName, List<ClassDependencyInfo> moduleClasses){
+        return new ModuleDependencyInfo(moduleName, moduleClasses);
+    }
 
     public Integer getInstability(){
         if(instability == null){
-            instability = Objects.requireNonNull(fanOut).size() / (Objects.requireNonNull(fanIn).size() + fanOut.size());
+            instability = Objects.requireNonNull(fanOutClasses).size() /
+                    (Objects.requireNonNull(fanInClasses).size() + fanOutClasses.size());
         }
 
         return instability;
+    }
+
+    public Integer getAbstractness(){
+        if(abstractness == null){
+            abstractness = Objects.requireNonNull(numOfAbsAndInterfaceClasses) / Objects.requireNonNull(numOfClasses);
+        }
+
+        return abstractness;
     }
 
 
---
+++ src/main/java/codeanalysis/ProjectDependencyInfo.java
@@ -0,0 +1,12 @@
+package codeanalysis;
+
+import java.util.List;
+
+public class ProjectDependencyInfo {
+    private String projectName;
+
+    private List<ModuleDependencyInfo> internalModuleInfo;
+    private List<ModuleDependencyInfo> thirdPartyModuleInfo;
+
+
+}
